6fa747dda191bc6659f9008d3eeffb3b93bd160a
mohuandao <1034255194@qq.com>
2019-08-06 16:02:29 +0800

    加上事物注解

--- src/main/java/com/example/demo/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/example/demo/springsecurity/SpringSecurityApplication.java
@@ -3,9 +3,11 @@ package com.example.demo.springsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableCaching //开启缓存
+@EnableTransactionManagement //开启事物管理
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
